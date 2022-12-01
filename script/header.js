9967a041c51958883b1b20139d727ee5173ab4f6
VickySonne <vst1996@live.dk>
2022-12-01 14:59:54 +0100

    burgermenu done for nu

--- script/header.js
+++ script/header.js
@@ -1,2 +1,15 @@
 window.addEventListener("DOMContentLoaded",load);
 
+function load (){
+    document.querySelector("#burgerMenuIcon").addEventListener("click",openBurgerMenu); 
+}
+
+function openBurgerMenu (){
+    let burgerMenu = document.querySelector("#burgerMenu");
+    if (burgerMenu.style.display === "flex") {
+        burgerMenu.style.display = "none";
+    }
+    else {
+        burgerMenu.style.display = "flex"
+    }
+}
