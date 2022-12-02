21d9b161eae5a00075c6c3f54eb3d9c6ba831227
Christian Pedersen <crpe44102@edu.ucl.dk>
2022-12-02 07:00:11 +0100

    recreate frontpage

--- script/header.js
+++ script/header.js
@@ -4,3 +4,19 @@ const burgerMenuIcon = document.querySelector("#burgerMenuIcon")
 const toggleBurgerMenu = () => burgerMenu.classList.toggle("hidden")
 
 burgerMenuIcon.addEventListener("click", toggleBurgerMenu)
+
+//  window.addEventListener("DOMContentLoaded",load);
+
+// function load (){
+//     document.querySelector("#burgerMenuIcon").addEventListener("click",openBurgerMenu); 
+// }
+
+// function openBurgerMenu (){
+//     let burgerMenu = document.querySelector("#burgerMenu");
+//     if (burgerMenu.style.display === "flex") {
+//         burgerMenu.style.display = "none";
+//     }
+//     else {
+//         burgerMenu.style.display = "flex"
+//     }
+// }
