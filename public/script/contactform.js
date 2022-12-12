908d35a59ff278515bb85ba62eeb5049a7dd8af8
Cassovich <cassws00@gmail.com>
2022-12-12 13:54:09 +0100

    Update contactform.js

--- public/script/contactform.js
+++ public/script/contactform.js
@@ -5,14 +5,11 @@ const phone = document.getElementById('phone')
 const form = document.getElementsById('form')
 const errorElement = document.getElementById('error')
 
-// form.addEventListener('submit', (e) => {
-//     let messages = []
-//     if (fullname.value === '' ) {
-//         messages.push('Navn er påkrævet')
-//     }
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (fullname.value === '' ) {
+        messages.push('Navn er påkrævet')
+    }
+    
+})
 
-//     if (messages.length > 0) {
-//         e.preventDefault()
-//         errorElement.innerText = messages.join(', ')
-//     }
-// })
