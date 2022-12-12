0b5d275e1ab43a4c069d5c623f71f9a750c57f89
Cassovich <cassws00@gmail.com>
2022-12-12 13:55:47 +0100

    Redo

--- public/script/contactform.js
+++ public/script/contactform.js
@@ -3,13 +3,12 @@ const corporation = document.getElementById('corporation')
 const email = document.getElementById('email')
 const phone = document.getElementById('phone')
 const form = document.getElementsById('form')
-const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
     if (fullname.value === '' ) {
         messages.push('Navn er påkrævet')
     }
-    
+
 })
 
