529509336000bc6f9d95745ca98b5f29a9739be2
Cassovich <cassws00@gmail.com>
2022-12-12 13:08:25 +0100

    Give Id

--- public/script/contactform.js
+++ public/script/contactform.js
@@ -1,4 +1,18 @@
-const name = document.getElementById('name')
+const fullname = document.getElementById('fullname')
+const corporation = document.getElementById('corporation')
 const email = document.getElementById('email')
 const phone = document.getElementById('phone')
 const form = document.getElementsById('form')
+const errorElement = document.getElementById('error')
+
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if (fullname.value === '' ) {
+//         messages.push('Navn er påkrævet')
+//     }
+
+//     if (messages.length > 0) {
+//         e.preventDefault()
+//         errorElement.innerText = messages.join(', ')
+//     }
+// })
