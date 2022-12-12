d47b272e5d72c730337bd448fe012db2a772debe
VickySonne <vst1996@live.dk>
2022-12-12 11:19:13 +0100

    Burgermenu åben close icon tilføjet

--- public/script/header.js
+++ public/script/header.js
@@ -7,4 +7,20 @@ const toggleBurgerMenu = () => burgerMenu.classList.toggle("hidden");
 
 function load (){
     burgerMenuIcon.addEventListener("click", toggleBurgerMenu);
+    burgerMenuIcon.addEventListener("click", changeIcon)
 }
+
+function changeIcon (){
+    burgerMenuIcon.classList.toggle("fa-bars")
+    burgerMenuIcon.classList.toggle("fa-x")
+}
+
+
+
+// const circle = 'fa-circle'
+// const check = 'fa-check-circle'
+// const toggler = document.getElementById('toggle')
+// toggle.onclick = () => {
+//   toggler.classList.toggle(circle)
+//   toggler.classList.toggle(check)
+// }
