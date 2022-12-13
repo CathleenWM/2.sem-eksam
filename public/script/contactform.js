617b70363c67973d9a9c418a05e12c84ad2cd6b8
Cassovich <cassws00@gmail.com>
2022-12-13 10:32:00 +0100

    JS

--- public/script/contactform.js
+++ public/script/contactform.js
@@ -13,4 +13,21 @@ form.addEventListener('submit', (e) => {
     if (fullname.value === '' ) {
         messages.push('Navn er påkrævet');
     }
+
+    let lastDot = email.value.lastIndexOf(".")
+    let lastAtSymbol = email.value.lastIndexOf("@")
+
+    if (lastDot === -1 || lastAtSymbol === -1) {
+        
+    }
+    // betydning = hvis ovenstående if ikke har fejl, så kør de andre if statements
+    else if (lastDot < email.value.length-1) {
+        
+    }
+    // betydning = @ skal være mindst 1 væk fra punktum
+    else if (lastAtSymbol < lastDot-1) {
+        
+    }
+
+
 })
