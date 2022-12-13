7e7265cec530eff8052ca30bf75d5f249a4dfd2b
Cassovich <cassws00@gmail.com>
2022-12-13 09:54:12 +0100

    Cass is dumb

--- public/script/contactform.js
+++ public/script/contactform.js
@@ -1,14 +1,16 @@
-const fullname = document.getElementById('fullname')
-const corporation = document.getElementById('corporation')
-const email = document.getElementById('email')
-const phone = document.getElementById('phone')
-const form = document.getElementsById('form')
 
-// form.addEventListener('submit', (e) => {
-//     let messages = []
-//     if (fullname.value === '' ) {
-//         messages.push('Navn er påkrævet')
-//     }
+const fullname = document.getElementById('fullname');
+const corporation = document.getElementById('corporation');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const form = document.getElementById('form')
 
-// })
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault
+    console.log(e);
+    let messages = [];
+    if (fullname.value === '' ) {
+        messages.push('Navn er påkrævet');
+    }
+});
